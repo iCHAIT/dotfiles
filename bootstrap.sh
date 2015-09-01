06405512194ffa0bb7763c7f86b7371448cebfbb
iCHAIT <cgupta319@gmail.com>
2015-09-01 23:51:12 +0530

    bashrc added

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ ln -f $DIR/git/gitconfig ~/.gitconfig
 
 # Sublime
 # /Users/chaitanyagupta/Library/Application Support/Sublime Text 3/Packages
-# ln -f $DIR/bash_profile ~/.bash_profile
-# ln -f $DIR/bash_profile ~/.bash_profile
+ln -f $DIR/bash/bash_profile ~/.bash_profile
+ln -f $DIR/bash/bashrc ~/.bashrc
