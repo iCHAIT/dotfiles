7bd5822d268e8267ae0ada87d6f6fbd42e36130e
iCHAIT <cgupta319@gmail.com>
2015-12-28 00:00:51 +0530

    started using zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,22 @@
+# Python
+alias py="python"
+alias py2="python2"
+alias py3="python3"
+
+# Virtualenv
+alias venv="virtualenv"
+
+# Serve a directory
+alias pysrv="python3 -m http.server "
+
+# https://github.com/Diaoul/subliminal
+alias sub="subliminal -l en -- "
+
+# Quick Ping!
+alias pingu="ping 8.8.8.8"
+
+# Misc.
+alias r="ranger"
+
+# postgresql
+alias psqld "pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
