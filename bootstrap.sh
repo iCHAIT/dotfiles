54dba8c0df373657797c9d01f5d6c488c733b0c5
iCHAIT <cgupta319@gmail.com>
2015-12-28 00:01:28 +0530

    more symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,9 @@ ln -f $DIR/git/gitconfig ~/.gitconfig
 # /Users/chaitanyagupta/Library/Application Support/Sublime Text 3/Packages
 ln -f $DIR/bash/bash_profile ~/.bash_profile
 ln -f $DIR/bash/bashrc ~/.bashrc
+
+
+# Zsh
+# wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+rm ~/.zshrc
+ln -sf $DIR/zsh/zshrc ~/.zshrc
