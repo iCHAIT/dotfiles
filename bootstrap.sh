c638a9f270e2749840b1b8bc55f98b70c6c5e0de
Chaitanya Gupta <cgupta319@gmail.com>
2026-07-04 18:50:13 +0800

    Remove unused bash and sublime configs
    Dead configs from ~2015 (Python 2.7, Heroku Toolbelt, Sencha Cmd).
    Now on zsh/fish; drop the bash linking lines from bootstrap.sh too.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,6 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Git
 ln -f $DIR/git/gitconfig ~/.gitconfig
 
-# Sublime
-# /Users/chaitanyagupta/Library/Application Support/Sublime Text 3/Packages
-ln -f $DIR/bash/bash_profile ~/.bash_profile
-ln -f $DIR/bash/bashrc ~/.bashrc
-
-
 # Zsh
 # wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 rm ~/.zshrc
